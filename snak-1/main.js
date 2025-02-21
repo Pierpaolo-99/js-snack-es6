95cf866fcda2d9cc53a1dc7b9c1aa0ccdfe25f0b
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-21 14:55:04 +0100

    first commit

---
+++ snak-1/main.js
@@ -0,0 +1,55 @@
+/* Traccia: 
+
+Snack 1
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+
+Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+
+// object array initialization
+const bike = [
+    {
+        nameBike: 'Endurace 7 RAW',
+        weight: '20'
+    },
+    {
+        nameBike: 'Neuron CF 8',
+        weight: '25'
+    },
+    {
+        nameBike: 'Neuron CF 8',
+        weight: '30'
+    },
+]
+
+// push the bike weight in array
+
+const arrBikeWeight = []
+
+for (let i = 0; i < bike.length; i++){
+    const thisBike = bike[i]
+    arrBikeWeight.push(thisBike.weight)
+}
+
+console.log(arrBikeWeight);
+
+// find the bike max weight 
+
+let max = 0;
+
+for (let i = 1; i < arrBikeWeight.length; i++) {
+  if (arrBikeWeight[i] > max) {
+    max = arrBikeWeight[i];
+  }
+}
+console.log(max);
+
+// stamp the bike with max weight in page
+
+
+
